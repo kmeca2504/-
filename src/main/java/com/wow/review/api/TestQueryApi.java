48d334756ce7094980a56c11b3d8ca70b58b76ec
wow <kmeca2504@naver.com>
2023-07-26 15:30:46 +0900

    first commit

--- src/main/java/com/wow/review/api/TestQueryApi.java
+++ src/main/java/com/wow/review/api/TestQueryApi.java
@@ -21,6 +21,6 @@ public class TestQueryApi {
 
     @GetMapping("/test/query/querydsl")
     public List<TestEntity>queryQuerydsl(){
-        return testService.findALL
+        return testService.findAllByNameByQuerydsl("wow");
     }
 }
